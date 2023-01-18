9fe86012adc3a3334b5d44c9879543bbceea9e9d
Octavian Dinu <dinuoctavianstefan@gmail.com>
2023-01-18 22:56:56 +0200

    Add PostController

---
+++ Controllers/PostController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Postr.Models;
+using Postr.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Postr.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostGeneratorService _postGeneratorService;
+
+        public PostController(IPostGeneratorService postGeneratorService)
+        {
+            _postGeneratorService = postGeneratorService;
+        }
+
+        // GET: api/<PostController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PostController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PostController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PostViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _postGeneratorService.GeneratePost(model.Input);
+
+                if (result.IsSuccess)
+                {
+                    return Ok(result);
+                }
+
+                return BadRequest(result);
+            }
+
+            return BadRequest("Somethin went wrong");
+        }
+
+        // PUT api/<PostController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PostController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
